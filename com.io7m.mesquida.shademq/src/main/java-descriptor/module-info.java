e761ec70e3d6ac002f37b5bf907657edd1651c8c
Mark Raynsford <code@io7m.com>
2025-04-04 18:50:41 +0000

    Fix module descriptor again.

--- com.io7m.mesquida.shademq/src/main/java-descriptor/module-info.java
+++ com.io7m.mesquida.shademq/src/main/java-descriptor/module-info.java
@@ -1,8 +1,12 @@
 open module com.io7m.mesquida.shademq
 {
+  requires java.desktop;
+  requires java.logging;
   requires java.naming;
+  requires java.sql;
   requires org.slf4j;
 
+  uses org.apache.activemq.artemis.spi.core.remoting.ssl.SSLContextFactory;
   uses org.apache.commons.logging.LogFactory;
 
   exports javax.jms;
